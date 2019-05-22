b7f325f6f72fc4ee372543d919cebc50dfa0d992
Alton Johnson <alton.jx@gmail.com>
2019-05-22 13:09:59 -0500

    Initial commit.

--- leprechaun.rb
+++ leprechaun.rb
@@ -33,8 +33,8 @@ class Leprechaun
 	def initialize(netstat_results, ports)
 		@servers = {}
 		@clients = {}
-		@dport_mappings = []
-		@sport_mappings = []
+		@dest_port_mappings = []
+		@source_port_mappings = []
 
 		@data = File.open(netstat_results).read.split("\n")
 		if ports.include? ","
@@ -53,62 +53,58 @@ class Leprechaun
 		@data.each do |line|
 			routes = line.scan(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}:\d+\b/)
 			next unless !routes.empty?
-			sip = routes[0].split(":")[0] # source IP address
-			sport = routes[0].split(":")[1] # source port
-			dip = routes[1].split(":")[0] # destination IP address
-			dport = routes[1].split(":")[1] # destination port
+			source_ip = routes[0].split(":")[0] # source IP address
+			source_port = routes[0].split(":")[1] # source port
+			dest_ip = routes[1].split(":")[0] # destination IP address
+			dest_port = routes[1].split(":")[1] # destination port
 
-			next if dip == "0.0.0.0"
+			next if dest_ip == "0.0.0.0"
 
-			if line.include? "TCP"
-				protocol = "tcp"
-			else
-				protocol = "udp"
-			end
-
-			if !@ports.include? "all" and !@ports.include? "common"
-				if !@ports.include? dport
-					next
-				end
-			end
+			protocol = (line.include?("TCP") ? "tcp" : "udp")
 
 			well_known = [17,21,22,23,25,53,69,80,81,86,110,123,135,139,143,161,389,443,445,587,636,1311,1433,1434,1720,2301,2381,3306,3389,4443,47001,5060,5061,5432,5500,5900,5901,5985,5986,7080,8080,8081,8082,8089,8000,8180,8443]
 
 			if @ports.include? "common"
-				next unless well_known.include? dport.to_i
+				next unless well_known.include? dest_port.to_i
 			end
 
-			if @servers[dip].nil?  # avoid adding duplicates
-				server_hex = SecureRandom.hex(2) # reference this in the dot digraph
-				@servers[dip] = {:hex => "", :ports => {}, :client_count => 0}
-				@servers[dip][:hex] = "s#{server_hex}"
-				@digraph_headers += "\t#{@servers[dip][:hex]} [label = < <b>#{dip}</b> >, fillcolor=gold3, fontcolor=white, style=filled, shape=egg];\n"
-			end
-			if @servers[dip][:ports]["#{dport}/#{protocol}"].nil?
-				@servers[dip][:ports]["#{dport}/#{protocol}"] = {:clients => [], :client_count => 0}
+			if !@ports.include? "all" and !@ports.include? "common"
+				if !@ports.include? dest_port
+					next
+				end
 			end
 
-			unless @servers[dip][:ports]["#{dport}/#{protocol}"][:clients].include? sip
-				@servers[dip][:ports]["#{dport}/#{protocol}"][:clients] << sip # add source IP
-				@servers[dip][:ports]["#{dport}/#{protocol}"][:client_count] += 1
-				@servers[dip][:client_count] += 1
+			if @servers[dest_ip].nil?  # avoid adding duplicate connections
+				server_hex = SecureRandom.hex(2)
+				@servers[dest_ip] = {:hex => "", :ports => {}, :client_count => 0}
+				@servers[dest_ip][:hex] = "s#{server_hex}"
+				@digraph_headers += "\t#{@servers[dest_ip][:hex]} [label = < <b>#{dest_ip}</b> >, fillcolor=gold3, fontcolor=white, style=filled, shape=egg];\n"
+			end
+			if @servers[dest_ip][:ports]["#{dest_port}/#{protocol}"].nil?
+				@servers[dest_ip][:ports]["#{dest_port}/#{protocol}"] = {:clients => [], :client_count => 0}
 			end
 
-			if @clients[sip].nil? # avoid adding duplicates
+			unless @servers[dest_ip][:ports]["#{dest_port}/#{protocol}"][:clients].include? source_ip
+				@servers[dest_ip][:ports]["#{dest_port}/#{protocol}"][:clients] << source_ip # add source IP
+				@servers[dest_ip][:ports]["#{dest_port}/#{protocol}"][:client_count] += 1
+				@servers[dest_ip][:client_count] += 1
+			end
+
+			if @clients[source_ip].nil? # avoid adding duplicate connections
 				client_hex = SecureRandom.hex(2)
-				@clients[sip] = "c#{client_hex}"
-				@digraph_headers += "\t#{@clients[sip]} [label = \"#{sip}\", fillcolor=green3, style=filled];\n"
+				@clients[source_ip] = "c#{client_hex}"
+				@digraph_headers += "\t#{@clients[source_ip]} [label = \"#{source_ip}\", fillcolor=green3, style=filled];\n"
 			end
 
-			if @dport_mappings.include? [dip, "#{dport}/#{protocol}"]
-				unless @sport_mappings.include? [sip, "#{dport}/#{protocol}"]
-					@digraph_data += "\t\"#{@clients[sip]}\" -> \"#{dport}/#{protocol}\";\n"
-					@sport_mappings << [sip, "#{dport}/#{protocol}"]
+			if @dest_port_mappings.include? [dest_ip, "#{dest_port}/#{protocol}"]
+				unless @source_port_mappings.include? [source_ip, "#{dest_port}/#{protocol}"]
+					@digraph_data += "\t\"#{@clients[source_ip]}\" -> \"#{dest_port}/#{protocol}\";\n"
+					@source_port_mappings << [source_ip, "#{dest_port}/#{protocol}"]
 				end
 			else
-				@dport_mappings << [dip, "#{dport}/#{protocol}"]
-				@sport_mappings << [sip, "#{dport}/#{protocol}"]
-				@digraph_data += "\t\"#{@clients[sip]}\" -> \"#{dport}/#{protocol}\" -> #{@servers[dip][:hex]};\n"
+				@dest_port_mappings << [dest_ip, "#{dest_port}/#{protocol}"]
+				@source_port_mappings << [source_ip, "#{dest_port}/#{protocol}"]
+				@digraph_data += "\t\"#{@clients[source_ip]}\" -> \"#{dest_port}/#{protocol}\" -> #{@servers[dest_ip][:hex]};\n"
 			end
 		end
 
