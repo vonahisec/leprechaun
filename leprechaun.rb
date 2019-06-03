143d808d75c9d46bc643a215e77415e42e8e705c
Alton Johnson <alton.jx@gmail.com>
2019-06-03 16:35:20 -0500

    Added error check for invalid file

--- leprechaun.rb
+++ leprechaun.rb
@@ -146,8 +146,9 @@ class Leprechaun
 	end
 	def write_to_file
 		File.open("data.dot", "w") {|f| f.write(@digraph)}
-		`sfdp -Tpng data.dot -o network_diagram.png -Grankdir=LR; rm data.dot`
-		puts "\n [*] Completed! Graph output file located at: ./network_diagram.png\n\n"
+		tmp_file = SecureRandom.hex(8)
+		`sfdp -Tpng data.dot -o #{tmp_file}.png -Grankdir=LR; rm data.dot`
+		puts "\n [*] Completed! Graph output file located at: ./#{tmp_file}.png\n\n"
 	end
 end
 if $0 == __FILE__
